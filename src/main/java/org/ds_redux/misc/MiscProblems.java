18619574ec63908a29172c9a55f63ebb5c74a7f9
Siddharth <sidcool234@gmail.com>
2022-10-31 16:10:17 +0530

    Some pretty fairly simple code to begin with.  A small step in a long journey

--- src/main/java/org/ds_redux/misc/MiscProblems.java
+++ src/main/java/org/ds_redux/misc/MiscProblems.java
@@ -7,4 +7,23 @@ public class MiscProblems {
         if (i == 0) return 0;
         return i + addNaturalNumbersTill(i - 1);
     }
+
+    public static FIZZ_BUZZ_RETURN_VALUES fizzBuzz(int i) {
+        if (i % 15 == 0) return FIZZ_BUZZ_RETURN_VALUES.FIZZ_BUZZ;
+        else if (i % 5 == 0) return FIZZ_BUZZ_RETURN_VALUES.BUZZ;
+        else if (i % 3 == 0) return FIZZ_BUZZ_RETURN_VALUES.FIZZ;
+        else return FIZZ_BUZZ_RETURN_VALUES.NONE;
+    }
+
+    public static boolean searchWithinString(String input, String searchTerm, boolean caseSensitive) {
+        if (input == null || searchTerm == null) return false;
+        var finalInput = caseSensitive ? input : input.toLowerCase();
+        var finalSearchString = caseSensitive ? searchTerm : searchTerm.toLowerCase();
+
+        return finalInput.contains(finalSearchString);
+    }
+}
+
+enum FIZZ_BUZZ_RETURN_VALUES {
+    FIZZ, BUZZ, FIZZ_BUZZ, NONE
 }
--- src/test/java/org/ds_redux/misc/MiscProblemsTest.java
+++ src/test/java/org/ds_redux/misc/MiscProblemsTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class MiscProblemsTest {
 
     @Test
-    public void shouldAddNaturalNumbersTill(){
+    public void shouldAddNaturalNumbersTill() {
         assertEquals(MiscProblems.addNaturalNumbersTill(5), 15);
         assertEquals(MiscProblems.addNaturalNumbersTill(10), 55);
         assertEquals(MiscProblems.addNaturalNumbersTill(-1), -1);
@@ -16,4 +16,21 @@ class MiscProblemsTest {
         assertNotEquals(MiscProblems.addNaturalNumbersTill(7), 100);
     }
 
+    @Test
+    public void shouldSearchCaseSensitive() {
+        assertTrue(MiscProblems.searchWithinString("ABc", "B", true));
+        assertTrue(MiscProblems.searchWithinString("My name is John", "is", true));
+
+        assertFalse(MiscProblems.searchWithinString("My name is John", "john", true));
+        assertFalse(MiscProblems.searchWithinString("My name is John", "Mike", true));
+    }
+
+    @Test
+    public void shouldSearchCaseInSensitive() {
+        assertTrue(MiscProblems.searchWithinString("ABc", "b", false));
+        assertTrue(MiscProblems.searchWithinString("My name is John", "is", false));
+
+        assertTrue(MiscProblems.searchWithinString("My name is John", "john", false));
+        assertFalse(MiscProblems.searchWithinString("My name is John", "Mike", false));
+    }
 }
